31c8ade9d2ebad1ec9a33e62f119d8e374969f87
lmhtq <lmhtq1991@gmail.com>
2015-11-27 12:04:32 +0800

    socket

--- src/socket.c
+++ src/socket.c
@@ -136,3 +136,385 @@ free_socket(int cpu_id, int ep_id)
 
     return;
 }
+
+
+
+/* api's */
+/* is connected */
+inline int
+mmutcpd_is_connected(mmutcpd_manager_t mmt, tcp_stream_t cur)
+{
+    if (!cur || cur->state != TCP_ESTABLISHED) {
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+/* get socket error */
+inline int 
+get_socket_error(socket_map_t sk, void *optval, socklen_t *optlen)
+{
+    tcp_stream_t cur;
+
+    if (!sk->stream) {
+        errno = EBADF;
+        return -1;
+    }
+
+    cur = sk->stream;
+    if (cur->state == TCP_CLOSE) {
+        if (cur->close_reason == TCP_TIMEOUT ||
+            cur->close_reason == TCP_CONN_FAIL ||
+            cur->close_reason == TCP_CONN_LOST) {
+            *(int *)optval = ETIMEDOUT;
+            *optlen = sizeof(int);
+
+            return 0;
+        }
+    }
+
+    if (cur->state == TCP_CLOSE_WAIT ||
+        cur->state == TCP_CLOSE) {
+        if (cur->close_reason == TCP_RESET) {
+            *(int *)optval = ECONNRESET;
+            *optlen = sizeof(int);
+
+            return 0;
+        }
+    }
+
+    errno = ENOSYS;
+    return -1;
+
+}
+
+
+/* get sockopt */
+int 
+mmutcpd_getsockopt(int cpu_id, int sockid, int level, 
+    int optname, void *optval, socklen_t *optlen)
+{
+    mmutcpd_manager_t mmt[cpu_id];
+    socket_map_t      sk;
+
+    if (sockid < 0 || sockid >= m_config.max_concurrency) {
+        fprintf(stderr, 
+            "Socket id %d out of range.\n", sockid);
+        errno = EBADF;
+        return -1;
+    }
+
+    sk = &mmt->socketq[sockid];
+    if (sk->socktype == SOCK_UNUSED) {
+        fprintf(stderr, 
+            "Invalid socket id %d\n", sockid);
+        errno = EBADF;
+        return -1;
+    }
+
+    if (sk->socktype != SOCK_LISTENER && sk->socktype != SOCK_STREAM) {
+        fprintf(stderr, 
+            "Not a stream socket id %d\n", sockid);
+        errno = EBADF;
+        return -1;
+    }
+
+    if (level == SOL_SOCKET) {
+        if (optname == SO_ERROR) {
+            if (sk->socktype == SOCK_STREAM) {
+                return mmutcpd_get_socket_error(sk, optval, optlen);
+            }
+        }
+    }
+
+    errno = ENOSYS;
+    return -1;
+}
+
+
+/* set sockopt */
+int
+mmutcpd_setsockopt(int cpu_id, int sockid, int level, 
+    int optname, const void *optval, socklen_t optlen)
+{
+    mmutcpd_manager_t mmt = g_mmutcpd[cpu_id];
+    socket_map_t      sk;
+
+    if (sockid < 0 || sockid >= m_config.max_concurrency) {
+        fprintf(stderr, 
+            "Socket id %d out of range.\n", sockid);
+        errno = EBADF;
+        return -1;
+    }
+
+    sk = &mmt->socketq[sockid];
+    if (sk->socktype == SOCK_UNUSED) {
+        fprintf(stderr, 
+            "Invalid socket id %d\n", sockid);
+        errno = EBADF;
+        return -1;
+    }
+
+    if (sk->socktype != SOCK_LISTENER && sk->socktype != SOCK_STREAM) {
+        fprintf(stderr, 
+            "Not a stream socket id %d\n", sockid);
+        errno = EBADF;
+        return -1;
+    }
+
+    return 0;
+}
+
+
+/* set sock nonblock */
+int
+mmutcpd_setsock_nonblock(int cpu_id, int sockid)
+{
+    mmutcpd_manager_t mmt = g_mmutcpd[cpu_id];
+    socket_map_t      sk;
+
+    if (sockid < 0 || sockid >= m_config.max_concurrency) {
+        fprintf(stderr, 
+            "Socket id %d out of range.\n", sockid);
+        errno = EBADF;
+        return -1;
+    }
+
+    sk = &mmt->socketq[sockid];
+    if (sk->socktype == SOCK_UNUSED) {
+        fprintf(stderr, 
+            "Invalid socket id %d\n", sockid);
+        errno = EBADF;
+        return -1;
+    }
+
+    sk->socketq[sockid].opts |= NON_BLOCK;
+
+    return 0;
+}
+
+
+/* socket ioctl */
+mmutcpd_socket_ioctl(int cpu_id, int sockid, int request, void *argp)
+{
+    mmutcpd_manager_t mmt = g_mmutcpd[cpu_id];
+    socket_map_t      sk;
+    tcp_stream_t      cur;
+    tcp_ring_buffer_t rbuf;
+
+    if (sockid < 0 || sockid >= m_config.max_concurrency) {
+        fprintf(stderr, 
+            "Socket id %d out of range.\n", sockid);
+        errno = EBADF;
+        return -1;
+    }
+
+    sk = &mmt->socketq[sockid];
+    /* must be stream socket */
+    if (sk->socktype != SOCK_STREAM) {
+        fprintf(stderr, 
+            "Invalid socket id %d\n", sockid);
+        errno = EBADF;
+        return -1;
+    }
+
+    if (!argp) {
+        errno = EFAULT;
+        return -1;
+    }
+
+    if (request == FIONREAD) {
+        cur = sk->stream;
+        if (!cur) {
+            errno = EBADF
+            return -1;
+        }
+
+        rbuf = cur->rcv_var->rcvbuf;
+        if (rbuf) {
+            *(int *)argp = rbuf->merged_len;
+        } else {
+            *(int I)argp = 0;
+        }
+    } else {
+        errno = EINVAL;
+        return -1;
+    }
+
+    return 0;
+}
+
+
+/* create a socket */
+mmutcpd_socket(int cpu_id, int domain, int type, int protocol)
+{
+    mmutcpd_manager_t mmt = g_mmutcpd[cpu_id];
+    socket_map_t      sk;
+
+    if (!mmt) {
+        errno = ENFILE;
+        return -1
+    }
+
+    if (domain != AF_INET) {
+        errno = EAFNOSUPPORT;
+        return -1
+    }
+
+    if (type == SOCK_STREAM) {
+        type = SOCK_STREAM;
+    } else {
+        errno = EINVAL;
+        return -1;
+    }
+
+    sk = allocate_socket(cpu_id, type, FALSE);
+    if (!sk) {
+        errno = ENFILE;
+        return -1;
+    }
+
+    return sk->id;
+}
+
+
+/* socket bind */
+mmutcpd_bind(int cpu_id, int sockid, const struct sockaddr *addr, 
+    socklen_t addrlen)
+{
+    mmutcpd_manager_t  mmt = g_mmutcpd[cpu_id];
+    socket_map_t       sk;
+    struct sockaddr_in *addr_in;
+
+    if (!mmt) {
+        return -1;
+    }
+
+    if (sockid < 0 || sockid >= m_config.max_concurrency) {
+        fprintf(stderr, 
+            "Socket id %d out of range.\n", sockid);
+        errno = EBADF;
+        return -1;
+    }
+
+    sk = &mmt->socketq[sockid];
+    if (sk->socktype == SOCK_UNUSED) {
+        fprintf(stderr, 
+            "Invalid socket id %d\n", sockid);
+        errno = EBADF;
+        return -1;
+    }
+
+    if (sk->socktype != SOCK_LISTENER && sk->socktype != SOCK_STREAM) {
+        fprintf(stderr, 
+            "Not a stream socket id %d\n", sockid);
+        errno = EBADF;
+        return -1;
+    }
+
+    if (!addr) {
+        fprintf(stderr, 
+            "Socket id %d: empty address.\n", sockid);
+        errno = EINVAL;
+        return -2;
+    }
+
+    if (sk->opts & ADDR_BIND) {
+        fprintf(stderr, 
+            "Socket id %d address already bind for this socket.\n", sockid);
+        errno = EINVAL;
+        return -1;
+    }
+
+    /* just support AF_INET */
+    if (addr->sa_family != AF_INET || addrlen < sizeof(struct sockaddr_in)) {
+        fprintf(stderr, 
+            "Socket id %d: invalid arguments\n", sockid);
+        errno = EINVAL;
+        return -1;
+    }
+
+    /* TODO: check whether the address is in use */
+
+    addr_in = (struct sockaddr_in*)addr;
+    sk.saddr = *addr_in;
+    sk.opts |= ADDR_BIND;
+
+    return 0;
+}
+
+
+/* socket listen */
+mmutcpd_listen(int cpu_id, int sockid, int backlog)
+{
+    mmutcpd_manager_t mmt = g_mmutcpd[cpu_id];
+    socket_map_t      sk;
+    tcp_listener_t    listener;
+
+    if (!mmt) {
+        return -1;
+    }
+
+    if (sockid < 0 || sockid >= m_config.max_concurrency) {
+        fprintf(stderr, 
+            "Socket id %d out of range.\n", sockid);
+        errno = EBADF;
+        return -1;
+    }
+
+    sk = &mmt->socketq[sockid];
+    if (sk->socktype == SOCK_UNUSED) {
+        fprintf(stderr, 
+            "Invalid socket id %d\n", sockid);
+        errno = EBADF;
+        return -1;
+    }
+
+    if (sk->socktype == SOCK_STREAM) {
+        sk.socktype = SOCK_LISTENER;
+    }
+
+    if (sk->socktype != SOCK_LISTENER) {
+        fprintf(stderr, 
+            "Not a listening socket id %d\n", sockid);
+        errno = EBADF;
+        return -1;
+    }
+
+    if (backlog <= 0 || backlog > m_config.max_concurrency) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    listener = (tcp_listener_t)calloc(1, sizeof(struct tcp_listener));
+    if (!listener) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    listener->sockid =sockid;
+    listener->backlog = backlog;
+    listener->socket = sk;
+
+    if (pthread_cond_init(&listener->accept_cond, NULL)) {
+        perror("pthread_cond_init of listener->accept_cond\n");
+        return -1;
+    }
+
+    if (pthread_mutex_init(&listener->accept_lock, NULLULL)) {
+        perror("pthread_mutex_init of listener->accept_lock\n");
+        return -1;
+    }
+
+    listener->acceptq = create_stream_queue(backlog);
+    if (!listener->acceptq) {
+        errno = ENOMEM;
+        return -1;
+    }
+
+    sk.listener = listener;
+    mmt->listener = listener;
+
+    return 0;
+}
